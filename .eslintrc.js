0a14e8d61d022235ef32318501c74636e5200ba2
Matt Revell <mattrevell82@me.com>
2016-03-29 11:46:20 +0100

    Add eslint rules.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "max-len": [2, 140, 2],
+        "quote-props": [2, "consistent"],
+        "react/sort-comp": 0,
+    }
+};
