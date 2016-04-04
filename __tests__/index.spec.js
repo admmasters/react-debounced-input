e433cde3e4e018c4b3fe665cdcc659ab537dab85
Matt Revell <mattrevell82@me.com>
2016-04-04 12:47:27 +0100

    Improve tests - WIP.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,11 +1,10 @@
-/* global describe, it*/
 import React, { PropTypes } from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import { expect } from 'chai';
-import highlightedText from '../';
+import highlightedText from '../src/';
 
 const MockText = props => (<div>
-  <span>{ props.text }</span>
+  { props.children }
 </div>);
 
 MockText.propTypes = { text: PropTypes.string };
@@ -14,7 +13,23 @@ const HighlightedMockText = highlightedText(MockText);
 
 describe('highlightedTextDecorator', () => {
   it('should be able to highlight text', () => {
-    const wrapper = shallow(<HighlightedMockText text="Hello Example" highlightedText="Hello" />);
+    const wrapper = shallow(<HighlightedMockText highlightedText="Hello">Hello Example</HighlightedMockText>);
     expect(wrapper.html()).to.contain('<strong class="highlighted-text">Hello</strong>');
   });
+  
+  it('should be able to handle nested blocks of text', () => {
+    const wrapper = render(
+      <HighlightedMockText highlightedText="Example">
+        <div>Hello Example</div>
+        <div>
+          <span>Hello Example</span>
+        </div>
+        <div>Hello Example</div>
+      </HighlightedMockText>
+    );
+    expect(wrapper.find('.highlighted-text')).to.have.length(3);
+  });
 });
+
+// <div>
+//   <span><span>Hello Example</span></span><span><strong class="highlighted-text">Extra</strong><span> Example</span></span></div>
