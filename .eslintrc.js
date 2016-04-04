82f57ed52e4f187d711a6f16e8c0ca50c2b86139
Matt Revell <mattrevell82@me.com>
2016-04-04 12:44:21 +0100

    Use the eslint import plugin.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
     "parser": "babel-eslint",
-    "extends": "airbnb",
+    "extends": [
+      "airbnb",
+      "plugin:import/errors"
+    ],
     "plugins": [
-        "react"
+        "react",
+        "import"
     ],
     "rules": {
         "max-len": [2, 140, 2],
         "quote-props": [2, "consistent"],
         "react/sort-comp": 0,
-    }
+    },
+   "globals": {
+       "describe": false,
+       "it": false,
+   }
 };
