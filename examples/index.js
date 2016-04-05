5ff8ab32c39647cfebb90c40a2559e2544f805fb
Matt Revell <mattrevell82@me.com>
2016-04-05 15:11:56 +0100

    Update examples.

--- examples/index.js
+++ examples/index.js
@@ -1,43 +1,23 @@
 import './styles.css';
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import { Table, TableRow, TableCell } from './react-standard-table';
-import highlightedText from '../src/index';
+import highlightedText from '../src';
 
-const TABLE_COLUMNS = [
-  { columnTitle: 'Individual', property: 'name' },
-  { columnTitle: 'Phone(s)', property: 'telephone_mobile' },
-  { columnTitle: 'Address(s)', property: 'address_1' },
-  { columnTitle: 'DOB', property: result => result.dob },
-];
-
-const RESULTS = [
-  {
-    name: 'Matt Revell',
-    telephone_mobile: '01322 280669',
-    address_1: '110 Park Road, Dartford, DA1 1SZ',
-    dob: '23/03/1982',
-  },
-];
-
-const HighlightedTable = highlightedText(Table);
-
-const App = () => (
+const App = props => (
   <div>
-    <HighlightedTable
-      highlightedText="Matt"
-      config={TABLE_COLUMNS}
-    >
-      { RESULTS.map((result, i) => (
-        <TableRow key={`table-row-${i}`}>
-          <TableCell>{ result.name }</TableCell>
-          <TableCell>{ result.telephone_mobile }</TableCell>
-          <TableCell>{ result.address_1 }</TableCell>
-          <TableCell>{ result.dob }</TableCell>
-        </TableRow>
-      ))}
-    </HighlightedTable>
+    { props.children }
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+App.propTypes = {
+  children: PropTypes.node,
+};
+
+const HighlightedApp = highlightedText(App);
+
+ReactDOM.render(
+  <HighlightedApp
+    highlightedText="Example"
+  >
+    This is an Example of highlighted text
+  </HighlightedApp>, document.getElementById('app'));
