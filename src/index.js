ea4de4e36595a7b54f40d2f29f642239833aa948
Matt Revell <mattrevell82@me.com>
2016-04-06 12:59:34 +0100

    Added the ability to specify a matchClass via props.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { PropTypes } from 'react';
 import Highlighter from 'react-highlighter';
 
-const highlightFunc = highlightedText => children => {
+const highlightFunc = (highlightedText, matchClass) => children => {
   if (Array.isArray(children)) {
     return children.map((child, i) => (
       React.createElement(child.type, { key: `highlighted-child${i}`, ...child.props }, highlightFunc(highlightedText)(child))
     ));
   } else if (typeof children === 'string') {
     return (
-      <Highlighter search={highlightedText} matchClass="highlighted-text">
+      <Highlighter search={highlightedText} matchClass={ matchClass }>
         { children }
       </Highlighter>
     );
@@ -18,20 +18,26 @@ const highlightFunc = highlightedText => children => {
 
 const HighlightText = ComposedComponent => {
   const HighlightedText = props => {
-    const { children, highlightedText, ...other } = props;
+    const { children, highlightedText, matchClass, ...other } = props;
     if (!props.highlightedText || props.highlightedText.length === 0 || !children || children.length === 0) {
       return <ComposedComponent { ...props } />;
     }
-    const highlightedChildren = highlightFunc(highlightedText)(children);
+    const highlightedChildren = highlightFunc(highlightedText, matchClass)(children);
     return (
       <ComposedComponent { ...other }>
         { highlightedChildren }
       </ComposedComponent>
     );
   };
+
+  HighlightedText.defaultProps = {
+    matchClass: 'highlighted-text',
+  };
+
   HighlightedText.propTypes = {
     children: PropTypes.node,
     highlightedText: PropTypes.string,
+    matchClass: PropTypes.string,
   };
   return HighlightedText;
 };
