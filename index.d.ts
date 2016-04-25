d31f0798b039b75a20a5ee12d2ab01ee6b42b671
Matt Revell <mattrevell82@me.com>
2016-04-25 11:33:42 +0100

    Add typescript typings and use cross-env.

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import { Component, Props } from 'react';
+
+export interface ReactDebouncedInputProps extends Props<ReactDebouncedInput> {
+  type: string,
+  onChange?: Function,
+  debounce?: number,
+  onDebounce?: Function,
+  placeholder?: string,
+  className?: string,
+}
+
+export default class ReactDebouncedInput extends Component<ReactDebouncedInputProps, {}> { }
