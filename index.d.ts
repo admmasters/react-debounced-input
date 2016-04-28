4bcb55153e69983ed8ff974e5a883b474151aeeb
Matt Revell <mattrevell82@me.com>
2016-04-28 20:16:59 +0100

    Add value string prop to index.d.ts.

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,13 @@
 import { Component, Props } from 'react';
 
 export interface ReactDebouncedInputProps extends Props<ReactDebouncedInput> {
-  type: string,
-  onChange?: Function,
-  debounce?: number,
-  onDebounce?: Function,
-  placeholder?: string,
-  className?: string,
+  type?: string;
+  value?: string;
+  onChange?: Function;
+  debounce?: number;
+  onDebounce?: Function;
+  placeholder?: string;
+  className?: string;
 }
 
 export default class ReactDebouncedInput extends Component<ReactDebouncedInputProps, {}> { }
