643b4e7ea503ff5783adb69840ff1d328a6a0323
Matt Revell <mattrevell82@me.com>
2016-04-29 16:34:53 +0100

    Make sure tests now have a value prop.

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -5,21 +5,21 @@ import TextInputBox from '../index';
 
 test('should render an empty text input box', t => {
   const onChange = text => text;
-  const wrapper = shallow(<TextInputBox onChange={onChange} />)
+  const wrapper = shallow(<TextInputBox value="" onChange={onChange} />)
   const expected = '<input type="text" value=""/>';
   const result = wrapper.html().includes(expected);
   t.is(result, true);
 });
 
 test('should have optional placeholder text', t => {
-  const wrapper = shallow(<TextInputBox className="test-class" />);
+  const wrapper = shallow(<TextInputBox value="" className="test-class" />);
   const expected = 'class="test-class"';
   const result = wrapper.html().includes(expected);
   t.is(result, true);
 });
 
 test('should be able to have its class set', t => {
-  const wrapper = shallow(<TextInputBox className="test-class" />);
+  const wrapper = shallow(<TextInputBox value="" className="test-class" />);
   const expected = 'class="test-class"';
   const result = wrapper.html().includes(expected);
   t.is(result, true);
@@ -29,7 +29,7 @@ test('should capture text', t => {
   const onChange = text => {
     t.is(text, 'example');
   };
-  const wrapper = shallow(<TextInputBox onChange={onChange} />);
+  const wrapper = shallow(<TextInputBox value="" onChange={onChange} />);
   wrapper.find('input').simulate('change', { target: { value: 'example' } });
 });
 
@@ -39,6 +39,6 @@ test('should be able to return a debounced string', t => {
   const onDebounce = () => {
     t.is(savedText, 'example');
   };
-  const wrapper = mount(<TextInputBox onDebounce={onDebounce} onChange={onChange} />);
+  const wrapper = mount(<TextInputBox value="" onDebounce={onDebounce} onChange={onChange} />);
   wrapper.find('input').simulate('change', { target: { value: 'example' } });
 });
